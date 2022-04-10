44b13ffef17830ef9dcba901137a1af566c9ba46
James Pelster <captainswag101@gmail.com>
2022-04-10 12:03:37 -0700

    Rename Project, add auto-generated version number and C++ standard version

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "main.h"   // This will be created in generated_src folder at compile time
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "AGC++ version v" << AGCPlusPlus_VERSION_MAJOR << "." << AGCPlusPlus_VERSION_MINOR << std::endl;
+    return 0;
 }
