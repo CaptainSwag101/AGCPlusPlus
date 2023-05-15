6579b85ecbae4d37be78c9f700665acf904a79dc
James Pelster <captainswag101@gmail.com>
2023-05-15 13:16:46 -0700

    Change the unknown subinstruction message to print Z-1

--- src/block1/cpu.cpp
+++ src/block1/cpu.cpp
@@ -143,7 +143,7 @@ namespace agcplusplus::block1 {
             // Force STD2 on unimplemented subinstruction
             if (!found_good_subinstruction) {
                 current_subinstruction = subinstruction_data[0];
-                std::cout << "Replacing unknown subinstruction at " << std::oct << bank << "," << z << std::dec << " with STD2" << std::endl;
+                std::cout << "Replacing unknown subinstruction at " << std::oct << bank << "," << (z - 1) << std::dec << " with STD2" << std::endl;
             }
         }
 
