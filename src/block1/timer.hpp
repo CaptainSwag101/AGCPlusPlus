91c8c8c12fd30ce431406165adc42bc389c8d811
James Pelster <captainswag101@gmail.com>
2023-07-13 09:57:30 -0700

    Make timer's "total_ticks" counter public to help debug timing issues

--- src/block1/timer.hpp
+++ src/block1/timer.hpp
@@ -8,8 +8,9 @@ namespace agcplusplus::block1 {
         void execute_tick_batch();
         void tick();
 
-    private:
         uint64_t total_ticks = 0;
+
+    private:
         // Timing variables for batching execution for timing accuracy
         constexpr static uint64_t TIMEPULSES_PER_SECOND = 1024000;
         constexpr static uint64_t TIMEPULSES_PER_MILLISECOND = TIMEPULSES_PER_SECOND / 1000;
